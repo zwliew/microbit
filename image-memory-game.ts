eecfde553c3e3f9e8c1dcd9c00c051c5e3a0a3fd
Zhao Wei Liew <zhaoweiliew@gmail.com>
2019-01-14 10:44:24 +0800

    Add comments for tutorial purposes

--- image-memory-game.ts
+++ image-memory-game.ts
@@ -3,9 +3,13 @@ let pattern: number[] = []
 let listening = false
 listening = false
 game.setScore(1)
+// Loop the game for 3 rounds
 while (game.score() <= 3) {
+    // Display the current round
     basic.showString("R" + game.score())
     basic.pause(500)
+
+    // Generate a random pattern and display the image pattern
     pattern = [Math.randomRange(1, 5), Math.randomRange(1, 5), Math.randomRange(1, 5), Math.randomRange(1, 5), Math.randomRange(1, 5)]
     for (let value of pattern) {
         if (value == 1) {
@@ -30,6 +34,8 @@ while (game.score() <= 3) {
     basic.pause(200)
     basic.clearScreen()
     basic.showString("Go!")
+
+    // Listen for the user's inputs
     listening = true
     progress = 0
     while (listening) {
@@ -78,6 +84,8 @@ while (game.score() <= 3) {
                 game.gameOver()
             }
         }
+
+        // Has the user gotten the entire sequence correct?
         if (progress >= 5) {
             listening = false
             game.addScore(1)
@@ -85,6 +93,8 @@ while (game.score() <= 3) {
         basic.pause(50)
     }
 }
+
+// The user has beaten the game. Display the victory screen!
 basic.showLeds(`
     # # # # #
     # # # # #
--- pong.ts
+++ pong.ts
@@ -4,6 +4,7 @@ const params = {
 }
 
 const actions = {
+    // Initialize the current game state
     initialize: function (): State {
         return {
             puckPos: new Vector({
@@ -23,6 +24,7 @@ const actions = {
             moved: false
         }
     },
+    // Move the platform left
     moveLeft: function (state: State): State {
         if (state.moved) {
             return state
@@ -39,6 +41,7 @@ const actions = {
             moved: true
         }
     },
+    // Move the platform right
     moveRight: function (state: State): State {
         if (state.moved) {
             return state
@@ -55,6 +58,7 @@ const actions = {
             moved: true
         }
     },
+    // Advances the game state by 1 step
     tick: function (state: State): State {
         if (!state.running) {
             return state
@@ -67,17 +71,22 @@ const actions = {
         let running = state.running
         let moved = state.moved
 
+        // Update the position of the platform
         platformPos = state.platformPos.add(state.platformVel)
         if (platformPos.x > 3 || platformPos.x < 1) {
+            // Prevent the platform from exceeding the boundaries of the screen
             platformPos = state.platformPos
         }
 
+        // Reset the velocity of the platform
         platformVel = new Vector({
             x: 0,
             y: 0
         })
 
+        // Update the position of the puck
         puckPos = puckPos.add(puckVel)
+        // Prevent the puck from exceeding the horizontal boundaries of the screen
         if (puckPos.x < 0) {
             puckPos = puckPos.add(new Vector({
                 x: 2,
@@ -98,6 +107,7 @@ const actions = {
             })
         }
 
+        // Prevent the puck from exceeding the vertical boundaries of the screen
         if (puckPos.y < 0) {
             puckPos = puckPos.add(new Vector({
                 x: 0,
@@ -120,6 +130,7 @@ const actions = {
                 })
             }
         } else if (puckPos.y > platformPos.y) {
+            // The player failed to catch the puck. Game over!
             running = false
         }
 
@@ -138,36 +149,45 @@ const actions = {
 let s: State = actions.initialize()
 
 basic.forever(function () {
+    // Advance the game state
     s = actions.tick(s)
 
+    // Is the game over?
     if (!s.running) {
         basic.showIcon(IconNames.Sad)
         return
     }
 
+    // Clear the screen
     for (let x = 0; x < 5; x++) {
         for (let y = 0; y < 5; y++) {
             led.unplot(x, y)
         }
     }
 
+    // Draw the platform on the screen
     led.plot(s.platformPos.x, s.platformPos.y)
     led.plot(s.platformPos.x - 1, s.platformPos.y)
     led.plot(s.platformPos.x + 1, s.platformPos.y)
 
+    // Draw the puck on the screen
     led.plot(s.puckPos.x, s.puckPos.y)
 
+    // Delay the game loop to meet the desired frame rate
     basic.pause(Math.floor(1000 / params.fps))
 })
 
+// Handle left button presses
 input.onButtonPressed(Button.A, function () {
     s = actions.moveLeft(s)
 })
 
+// Handle right button presses
 input.onButtonPressed(Button.B, function () {
     s = actions.moveRight(s)
 })
 
+// Interface definitions for various classes used in the game
 interface State {
     puckPos: Vector
     platformPos: Vector
